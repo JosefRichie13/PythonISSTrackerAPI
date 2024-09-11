d1ef0f467e7c58059f231323bc9ed197580f9fb7
Joseph Richie <joseph.richie@qumu.com>
2024-09-11 13:41:23 +0530

    Added code comments

--- index.py
+++ index.py
@@ -5,23 +5,29 @@ from contextlib import asynccontextmanager
 import threading
 import time
 from fastapi.templating import Jinja2Templates
-from collections import deque 
+from collections import deque
 
 
-
-
+# Create a Deque [0,0,0,0,0,0,0,0,0,0] and length 10 to hold the coordinates.
+# This holds only the latest 10 coordinates and is a FIFO queue
 listOfCoordinates = deque([0] * 10, maxlen=10)
 
+
+# Create a Lifespan event
+# On the start of the App, a thread is created which calls the function, getCoordinates
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     threading.Thread(target=getCoordinates, daemon=True).start()
     yield
 
 
+# Initiate the App with Lifespan and templates
 app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="templates")
 
 
+# Configures CORS settings
+# Accepts CORS from any origin, method, headers
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
@@ -32,15 +38,23 @@ app.add_middleware(
 )
 
 
+# Landing Page
 @app.get("/")
 def landingPage():
     return {"status": "Hello"}
 
+# Function to generate ISS coordinates
+
 
 def getCoordinates():
-    issUrl = "http://api.open-notify.org/iss-now.json"
-    index = 0
 
+    # ISS URL which returns the coordinate details
+    issUrl = "http://api.open-notify.org/iss-now.json"
+
+    # Infinite loop, which requests the data from the URL
+    # Parses and extracts the details (Latitude, Longitude, Timestamp) that are needed and appends to the dequeue
+    # Sleeps for 60 seconds and loops again
+    index = 0
     while index < 1:
         try:
             requestData = requests.get(url=issUrl, timeout=50)
@@ -48,21 +62,17 @@ def getCoordinates():
         except:
             print("API Failed to respond")
         extractedData = requestData.json()
-        listOfCoordinates.appendleft({"latitude": extractedData["iss_position"]['latitude'], "longitude":extractedData["iss_position"]['longitude'], "timestamp": extractedData["timestamp"]})
+        listOfCoordinates.appendleft({"latitude": extractedData["iss_position"]['latitude'], "longitude": extractedData["iss_position"]['longitude'], "timestamp": extractedData["timestamp"]})
         time.sleep(60)
 
 
-
-
+# Returns the coordinates as a list
 @app.get("/getISSCoordinates")
 def getISSCoordinates():
     return list(listOfCoordinates)
 
 
-
-
-
+# Displays the map
 @app.get("/showISSMap")
-def showISSMap(request:Request):
+def showISSMap(request: Request):
     return templates.TemplateResponse(request=request, name="ISSVisual.html")
-
